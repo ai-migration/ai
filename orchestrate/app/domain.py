1c1ec5d51f2d54a217c06b46923dc5638770dc26
leeyumin <lee_yumin@naver.com>
2025-08-11 18:58:44 +0900

    api 요청 받는 부분 수정

--- orchestrate/app/domain.py
+++ orchestrate/app/domain.py
@@ -3,6 +3,12 @@ from dataclasses import dataclass
 @dataclass
 class ToTranslator:
     id: int
+    user_id: int
+    file_path: str
+    input_egov_frame_ver: str
+    output_egov_frame_ver: str
+    is_test_code: bool
+    conversion_type: str
 
 @dataclass
 class ToAuditor:
